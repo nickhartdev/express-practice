d73350b1a252a1174ee226a01ba5fea56a182a70
Nick Hart <nick.hart.des@gmail.com>
2020-09-03 13:07:50 -0500

    Add second GET handler

--- server.js
+++ server.js
@@ -4,18 +4,26 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pokemon Box';
 app.locals.pokemon = [
-  {id: 1, name: 'Bulbasaur', types: ['Grass', 'Poison']},
-  {id: 4, name: 'Charmander', types: ['Fire']},
-  {id: 7, name: 'Squirtle', types: ['Water']}
+  {id: '1', name: 'Bulbasaur', types: ['Grass', 'Poison']},
+  {id: '4', name: 'Charmander', types: ['Fire']},
+  {id: '7', name: 'Squirtle', types: ['Water']}
 ]
 
 app.get('/', (request, response) => {
-  response.send('OH HEY');
+  response.status(200).send('Welcome to your pokemon box!');
 })
 
 app.get('/api/v1/pokemon', (request, response) => {
   const pokemon = app.locals.pokemon;
-  response.json({ pokemon });
+  response.status(200).json({ pokemon });
+})
+
+app.get('/api/v1/pokemon/:id', (request, response) => {
+  const pokemonMatch = app.locals.pokemon.find(pokemon => {
+    return pokemon.id === request.params.id;
+  });
+
+  response.status(200).json({ pokemonMatch });
 })
 
 app.listen(app.get('port'), () => {
