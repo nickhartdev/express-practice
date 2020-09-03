7bbb76ef2955dea83d24e6a0bc1562a2b4bc5a13
Nick Hart <nick.hart.des@gmail.com>
2020-09-03 12:40:43 -0500

    Add data to app.locals

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pokemon Box';
 app.locals.pokemon = [
-  
+  {id: 1, name: 'Bulbasaur', types: ['Grass', 'Poison']},
+  {id: 2, name: 'Ivysaur', types: ['Grass', 'Poison']},
+  {id: 3, name: 'Venusaur', types: ['Grass', 'Poison']}
 ]
