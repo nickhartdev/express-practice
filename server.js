d8be98904b3d029472a4265e80279c615c4eda3f
Nick Hart <nick.hart.des@gmail.com>
2020-09-03 12:43:40 -0500

    Alter dataset to give more variety

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pokemon Box';
 app.locals.pokemon = [
   {id: 1, name: 'Bulbasaur', types: ['Grass', 'Poison']},
-  {id: 2, name: 'Ivysaur', types: ['Grass', 'Poison']},
-  {id: 3, name: 'Venusaur', types: ['Grass', 'Poison']}
+  {id: 4, name: 'Charmander', types: ['Fire']},
+  {id: 7, name: 'Squirtle', types: ['Water']}
 ]
+
+app.get()
