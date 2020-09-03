fa706f5e1aed6e47bba8ca174a97f18ad3fecb8e
Nick Hart <nick.hart.des@gmail.com>
2020-09-03 12:52:17 -0500

    Configure app.listen

--- server.js
+++ server.js
@@ -9,4 +9,15 @@ app.locals.pokemon = [
   {id: 7, name: 'Squirtle', types: ['Water']}
 ]
 
-app.get()
+app.get('/', (request, response) => {
+  response.send('OH HEY');
+})
+
+app.get('/api/v1/pokemon', (request, response) => {
+  const pokemon = app.locals.pokemon;
+  response.json({ pokemon });
+})
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+})
